0a204c82a75e809675c39cf393d2fb58fff52a68
GiorgiMegeneishvili <megeneishvili997@gmail.com>
2025-08-10 12:53:44 -0700

    Update LeetCode.sql

--- LeetCode.sql
+++ LeetCode.sql
@@ -363,3 +363,20 @@ FROM Courses
 GROUP BY class
 having COUNT(*) > 4;
 
+
+
+
+
+--------      607. Sales Person                        -----------------------------------
+
+
+SELECT s.name
+FROM SalesPerson AS s
+WHERE NOT EXISTS (
+    SELECT 1
+    FROM Orders AS o
+    JOIN Company AS c
+      ON c.com_id = o.com_id
+    WHERE o.sales_id = s.sales_id
+      AND c.name = 'RED'
+);
